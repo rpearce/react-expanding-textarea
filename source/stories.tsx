c8a00c6ce006498dc8a1c535a398f5074859e91f
Robert Pearce <me@robertwpearce.com>
2020-08-28 22:26:19 -0400

    Fix textareas with max-height not scrolling

--- source/stories.tsx
+++ source/stories.tsx
@@ -99,3 +99,34 @@ export const WithMinimum3Rows: FC = () => {
     </main>
   )
 }
+
+export const WithMaxHeight: FC = () => {
+  const textareaRef = useRef<HTMLTextAreaElement>()
+
+  const handleChange = useCallback((e) => {
+    console.log(e.target.value)
+  }, [])
+
+  useEffect(() => {
+    textareaRef.current?.focus()
+  }, [])
+
+  return (
+    <main>
+      <h1>Minimalist textarea</h1>
+      <label htmlFor="my-textarea">Please Enter Some Details:</label>
+      <Textarea
+        className="textarea"
+        defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        id="my-textarea"
+        maxLength={number('maxLength', 3000)}
+        name="pet[notes]"
+        onChange={handleChange}
+        placeholder={text('placeholder', 'Enter details here...')}
+        ref={textareaRef}
+        rows={text('rows', '1')}
+        style={{ display: 'block', maxHeight: '100px', width: '350px' }}
+      />
+    </main>
+  )
+}
