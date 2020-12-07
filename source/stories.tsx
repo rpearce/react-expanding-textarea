bf7ca3606f04aa1a159ee67eb488a1ca568fe294
Robert Pearce <me@robertwpearce.com>
2020-12-07 18:18:31 -0500

    Feature: support callback refs

--- source/stories.tsx
+++ source/stories.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useEffect, useRef } from 'react'
+import React, { ChangeEvent, Component, FC, useCallback, useEffect, useRef } from 'react'
 import { number, text } from '@storybook/addon-knobs'
 import '../.storybook/base.css'
 
@@ -129,4 +129,54 @@ export const WithMaxHeight: FC = () => {
       />
     </main>
   )
+}
+
+type FunctionRefProps = Record<string, never>
+
+interface FunctionRefState {
+  value: string
+}
+
+class FunctionRef extends Component<FunctionRefProps, FunctionRefState> {
+  el: HTMLTextAreaElement | null = null
+
+  constructor(props: FunctionRefProps) {
+    super(props)
+    this.state = {
+      value: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    }
+  }
+
+  handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    this.setState({ value: e.currentTarget.value })
+  }
+
+  setRef = (node: HTMLTextAreaElement): void => {
+    this.el = node
+  }
+
+  render() {
+    console.log(this.el)
+
+    return (
+      <main>
+        <h1>Regular textarea</h1>
+        <label htmlFor="my-textarea">Please Enter Some Details:</label>
+        <Textarea
+          id="my-textarea"
+          maxLength={number('maxLength', 3000)}
+          name="pet[notes]"
+          onChange={this.handleChange}
+          placeholder={text('placeholder', 'Enter details here...')}
+          ref={this.setRef}
+          style={{ display: 'block', width: '300px' }}
+          value={this.state.value}
+        />
+      </main>
+    )
+  }
+}
+
+export const WithFunctionRef: FC = () => {
+  return <FunctionRef />
 }
