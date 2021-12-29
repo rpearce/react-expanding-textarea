d1b7910f6047d4ba72b2a4f0fc92351b3c9b1d99
Robert W. Pearce <me@robertwpearce.com>
2021-12-29 14:04:34 -0500

    Update ToggleWidth story name and add example of changing the textarea's width, itself

--- source/stories.tsx
+++ source/stories.tsx
@@ -215,40 +215,63 @@ export const ValueFromProps: FC = () => {
   )
 }
 
-export const ToggleWidth: FC = () => {
-  const [isWide, setIsWide] = useState(false)
+export const StyleChanges: FC = () => {
+  const [isWide0, setIsWide0] = useState(false)
+  const [isWide1, setIsWide1] = useState(false)
 
-  const handleClickToggle = useCallback(() => {
-    setIsWide(x => !x)
+  const handleClickToggle0 = useCallback(() => {
+    setIsWide0(x => !x)
+  }, [])
+
+  const handleClickToggle1 = useCallback(() => {
+    setIsWide1(x => !x)
   }, [])
 
   return (
     <main>
-      <h1>Toggle the textarea&apos;s width</h1>
-      <p>
-        When it goes from smaller to larger, there should not be any extra
-        whitespace leftover at the bottom from its height when it was small.
-      </p>
-      <p>
-        This will only work if <code>ResizeObserver</code> is available in your
-        browser.
-      </p>
-      <button onClick={handleClickToggle} type="button">
-        Toggle textarea width
-      </button>
-      <div>
-        <label htmlFor="my-textarea">Please Enter Some Details:</label>
-        <div style={{ width: isWide ? 400 : 200 }}>
+      <h1>Textarea&apos;s width / style changes</h1>
+      <section>
+        <h2>Toggling the parent&apos;s width</h2>
+        <p>
+          When it goes from smaller to larger, there should not be any extra
+          whitespace leftover at the bottom from its height when it was small.
+          This will only work if <code>ResizeObserver</code> is available in
+          your browser.
+        </p>
+        <button onClick={handleClickToggle0} type="button">
+          Toggle textarea parent&apos;s width
+        </button>
+        <div>
+          <label htmlFor="my-textarea0">Please Enter Some Details:</label>
+          <div style={{ width: isWide0 ? 400 : 200 }}>
+            <Textarea
+              defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dsa
+das
+d
+dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est labor"
+              id="my-textarea0"
+              style={{ display: 'block', width: '100%' }}
+            />
+          </div>
+        </div>
+      </section>
+      <section>
+        <h2>Toggling the textarea&apos;s width</h2>
+        <button onClick={handleClickToggle1} type="button">
+          Toggle textarea width
+        </button>
+        <div>
+          <label htmlFor="my-textarea1">Please Enter Some Details:</label>
           <Textarea
             defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dsa
 das
 d
 dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est labor"
-            id="my-textarea"
-            style={{ display: 'block', width: '100%' }}
+            id="my-textarea1"
+            style={{ display: 'block', width: isWide1 ? 400 : 200 }}
           />
         </div>
-      </div>
+      </section>
     </main>
   )
 }
